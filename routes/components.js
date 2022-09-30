fe373eab31ece0bce462fbef40a82ccbddb2877c
Jonathan Adel <joathanadel98@gmail.com>
2022-09-30 15:49:40 +0200

    Commit

--- routes/components.js
+++ routes/components.js
@@ -4,5 +4,17 @@ import Component from '../models/Component.js';
 const router = express.Router();
 
 
+// Fetching components by building id
+router.get('/:id', async (req, res) => {
+    try {
+        const buildingId = req.params.id;
+        const components = await Component.find({building_code:buildingId});
+        res.status(200).json(components);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 // Export
 export default router;
