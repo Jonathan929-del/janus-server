ac41ed89bc506ae91b8f8687af2536fb81275f19
Jonathan Adel <joathanadel98@gmail.com>
2022-09-30 11:02:06 +0200

    Commit

--- routes/buildings.js
+++ routes/buildings.js
@@ -1,6 +1,19 @@
 // Imports
 import express from 'express';
+import Buildings from '../../client/components/Buildings';
 const router = express.Router();
+
+
+// Fetching buildings by property id
+router.get('/:id', async (req, res) => {
+    try {
+        const propertyId = req.params.id;
+        const buildings = await Buildings.find({propertyCode:propertyId});
+        res.status(200).json(buildings);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 // Export
