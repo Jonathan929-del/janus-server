9e93bacbe84edf94cc65e4128523a0e2dfceda46
Jonathan Adel <joathanadel98@gmail.com>
2022-10-01 18:00:21 +0200

    Commit

--- routes/activities.js
+++ routes/activities.js
@@ -1,6 +1,19 @@
 // Imports
 import express from 'express';
 const router = express.Router();
+import Activities from '../models/Activity.js';
+
+
+// Fetching activities by component code
+router.get('/:id', async (req, res) => {
+    try {
+        const componentCode = req.params.id;
+        const activities = await Activities.find({component:componentCode});
+        res.status(200).json(activities);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 // Export
