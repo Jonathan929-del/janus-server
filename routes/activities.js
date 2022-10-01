b198d495695c6081b4f54299a5a72eba23522f40
Jonathan Adel <joathanadel98@gmail.com>
2022-10-01 18:41:28 +0200

    Commit

--- routes/activities.js
+++ routes/activities.js
@@ -16,5 +16,17 @@ router.get('/:id', async (req, res) => {
 });
 
 
+// Fetching activity by id
+router.get('/activity/:id', async (req, res) => {
+    try {
+        const activityId = req.params.id;
+        const activity = await Activities.findById(activityId);
+        res.status(200).json(activity);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 // Export
 export default router;
