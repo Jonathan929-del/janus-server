ed571425890f914abb09f2874864eaf0e22b3298
Jonathan Adel <joathanadel98@gmail.com>
2022-10-06 10:40:51 +0200

    Commit

--- routes/activities.js
+++ routes/activities.js
@@ -1,14 +1,14 @@
 // Imports
 import express from 'express';
 const router = express.Router();
-import Activities from '../models/Activity.js';
+import Activity from '../models/Activity.js';
 
 
 // Fetching activities by component code
 router.get('/:id', async (req, res) => {
     try {
         const componentCode = req.params.id;
-        const activities = await Activities.find({component:componentCode});
+        const activities = await Activity.find({component:componentCode});
         res.status(200).json(activities);
     } catch (err) {
         res.status(500).json(err);
@@ -20,8 +20,39 @@ router.get('/:id', async (req, res) => {
 router.get('/activity/:id', async (req, res) => {
     try {
         const activityId = req.params.id;
-        const activity = await Activities.findById(activityId);
+        const activity = await Activity.findById(activityId);
         res.status(200).json(activity);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// Posting activity
+router.post('/', async (req, res) => {
+    try {
+        const activity = await Activity.create({
+            date:req.body.date,
+            user:req.body.user,
+            description:req.body.description,
+            activity:req.body.activity,
+            work_order:req.body.work_order,
+            time:req.body.time,
+            article_number:req.body.article_number,
+            quantaty:req.body.quantaty,
+            remark:req.body.remark,
+            time_minutes:req.body.time_minutes,
+            material_coast:req.body.material_coast,
+            longitude:req.body.longitude,
+            latitude:req.body.latitude,
+            building:req.body.building,
+            property:req.body.property,
+            component:req.body.component,
+            unique_index_component:req.body.unique_index_component,
+            changed_by:req.body.changed_by,
+            change_date:req.body.change_date
+        });
+        res.status(201).json(activity);
     } catch (err) {
         res.status(500).json(err);
     }
