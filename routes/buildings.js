2b2d9d01114f35924625c880e8435c5d0dc3e784
Jonathan Adel <joathanadel98@gmail.com>
2022-10-17 18:23:02 +0200

    Commit

--- routes/buildings.js
+++ routes/buildings.js
@@ -1,6 +1,6 @@
 // Imports
 import express from 'express';
-import Buildings from '../models/Building.js';
+import Building from '../models/Building.js';
 const router = express.Router();
 
 
@@ -8,8 +8,20 @@ const router = express.Router();
 router.get('/:id', async (req, res) => {
     try {
         const propertyId = req.params.id;
-        const buildings = await Buildings.find({property_code:propertyId});
+        const buildings = await Building.find({property_code:propertyId});
         res.status(200).json(buildings);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// Fetching building by id
+router.get('/building-id/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const building = await Building.findById(id);
+        res.status(200).json(building);
     } catch (err) {
         res.status(500).json(err);
     }
