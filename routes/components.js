18cb5fe9a08668efb98824a8aabfa2e9facf704c
Jonathan Adel <joathanadel98@gmail.com>
2022-10-20 01:53:53 +0200

    Commut

--- routes/components.js
+++ routes/components.js
@@ -45,7 +45,7 @@ router.delete('/:id', async (req, res) => {
     try {
         const id = req.params.id;
         await Component.findByIdAndDelete(id);
-        res.json(200).json('Component deleted');
+        res.status(200).json('Component deleted');
     } catch (err) {
         res.status(500).json(err);
     }
