af1321ec5be0408bd471bafc92ef6b66a636d0b1
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2022-11-03 19:35:27 +0200

    Commit

--- routes/notifications.js
+++ routes/notifications.js
@@ -8,8 +8,8 @@ import Notification from '../models/Notification.js';
 // Fetching components with notifications component codes
 router.get('/', async (req, res) => {
     try {
-        const notifications = await Notification.find();
-        const components = await Promise.all(notifications.map(notification => {
+        const allNotifications = await Notification.find();
+        const components = await Promise.all(allNotifications.map(notification => {
             return Component.find({component_code:notification.component_code});
         }));
         res.status(200).json(components);
